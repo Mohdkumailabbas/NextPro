c4008e94aebd985cb4fcf543194d3061130511d5
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2026-04-22 22:56:37 +0530

    09 file-explorer

--- src/features/projects/components/project-id-view.tsx
+++ src/features/projects/components/project-id-view.tsx
@@ -2,10 +2,16 @@
 "use client"
 import { Id } from "../../../../convex/_generated/dataModel"
 import { FaGithub } from "react-icons/fa";
-
+import { Allotment } from "allotment";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
+import { FileExplorer } from "./file-explorer";
 
+
+const MIN_SIDEBAR_WIDTH = 200;
+const MAX_SIDEBAR_WIDTH = 800;
+const DEFAULT_SIDEBAR_WIDTH = 350;
+const DEFAULT_MAIN_SIZE = 1000;
 
 const Tab = ({
   label,
@@ -34,8 +40,9 @@ export const ProjectIdView = ({
 }: { 
   projectId: Id<"projects">
 }) => {
-    const [activeView, setActiveView] = useState<"editor" | "preview">("editor");
-     return (
+  const [activeView, setActiveView] = useState<"editor" | "preview">("editor");
+
+  return (
     <div className="h-full flex flex-col">
       <nav className="h-8.75 flex items-center bg-sidebar border-b">
         <Tab
@@ -60,7 +67,19 @@ export const ProjectIdView = ({
           "absolute inset-0",
           activeView === "editor" ? "visible" : "invisible"
         )}>
-          <div>Editor</div>
+          <Allotment defaultSizes={[DEFAULT_SIDEBAR_WIDTH, DEFAULT_MAIN_SIZE]}>
+            <Allotment.Pane
+              snap
+              minSize={MIN_SIDEBAR_WIDTH}
+              maxSize={MAX_SIDEBAR_WIDTH}
+              preferredSize={DEFAULT_SIDEBAR_WIDTH}
+            >
+              <FileExplorer projectId={projectId} />
+            </Allotment.Pane>
+            <Allotment.Pane>
+              <p>Editor view</p>
+            </Allotment.Pane>
+          </Allotment>
         </div>
         <div className={cn(
           "absolute inset-0",
